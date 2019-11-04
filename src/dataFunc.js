1a3dcfd23702472bf261c359cda5c87a122dbfed
nourbenhassen <nourbenhassen@gmail.com>
2019-11-04 15:36:25 +0100

    cleaner code

--- src/dataFunc.js
+++ src/dataFunc.js
@@ -2,15 +2,16 @@ const csv = require("csv-parser");
 const fs = require("fs");
 
 //function calculates distance between two points given latitudes and longitudes
+//https://www.geodatasource.com/developers/javascript
 function distanceFunc(lat1, lon1, lat2, lon2) {
-  if (lat1 == lat2 && lon1 == lon2) {
+  if (lat1 === lat2 && lon1 === lon2) {
     return 0;
   } else {
-    var radlat1 = (Math.PI * lat1) / 180;
-    var radlat2 = (Math.PI * lat2) / 180;
-    var theta = lon1 - lon2;
-    var radtheta = (Math.PI * theta) / 180;
-    var dist =
+    const radlat1 = (Math.PI * lat1) / 180;
+    const radlat2 = (Math.PI * lat2) / 180;
+    const theta = lon1 - lon2;
+    const radtheta = (Math.PI * theta) / 180;
+    let dist =
       Math.sin(radlat1) * Math.sin(radlat2) +
       Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
     if (dist > 1) {
@@ -19,6 +20,7 @@ function distanceFunc(lat1, lon1, lat2, lon2) {
     dist = Math.acos(dist);
     dist = (dist * 180) / Math.PI;
     dist = dist * 60 * 1.1515;
+    dist = dist * 1.609344; //convert from miles to KM
     return dist;
   }
 }
